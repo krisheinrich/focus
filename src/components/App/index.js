2b543b7b080ba8fcdf6e70703c932ef95b151d38
Kris Heinrich <krisheinrich@gmail.com>
2020-09-27 15:38:12 -0700

    Add default styles; remove supefluous wrapping div; make app root fill screen space

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,7 +6,7 @@ import LandingPage from '../Landing';
 import SignUpPage from '../SignUp';
 import SignInPage from '../SignIn';
 import PasswordForgetPage from '../PasswordForget';
-import HomePage from '../Home';
+import FocusPage from '../Focus';
 import AccountPage from '../Account';
 import AdminPage from '../Admin';
 
@@ -15,19 +15,18 @@ import { withAuthentication } from '../Session';
 
 const App = () => (
   <Router>
-    <div>
-      <Navigation />
+    <Navigation />
 
-      <hr />
+    <hr />
 
-      <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-      <Route path={ROUTES.HOME} component={HomePage} />
-      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route path={ROUTES.ADMIN} component={AdminPage} />
-    </div>
+    <Route exact path={ROUTES.LANDING} component={LandingPage} />
+    <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+    <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+    <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+
+    <Route path={ROUTES.HOME} component={FocusPage} />
+    <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+    <Route path={ROUTES.ADMIN} component={AdminPage} />
   </Router>
 );
 
